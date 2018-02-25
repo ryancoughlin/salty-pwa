7d6c4217201dc2a89728e588b5b194fb883d630f
Ryan Coughlin <ryan@ryancoughlin.com>
2018-02-24 20:59:12 -0500

    Add in proper font page to Inter UI

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <pre>{JSON.stringify(this.state.tides)}</pre>
+        <h2>{JSON.stringify(this.state.tides)}</h2>
       </div>
     )
   }
