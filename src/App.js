58964e49860bea121ade2ccab149d330b27c9be4
Ryan Coughlin <ryan@ryancoughlin.com>
2018-02-27 20:48:49 -0500

    Apply base to build out routing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 import React, { Component } from 'react'
-import './App.css'
-import request from './utils/request'
-import findNextTide from './utils/find-next-tide'
-import { fetchLocation } from './utils/location'
-import TidePhrase from './location/tide-phrase'
-import CurrentWeather from './location/current-weather'
-import TodaysTides from './location/todays-tides'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Location from './location/index'
 
 class App extends Component {
-  state = {
-    tides: JSON.parse(localStorage.getItem('tides')),
-    weather: JSON.parse(localStorage.getItem('weather')),
-  }
-
-  componentDidMount() {
-    // Get user location
-    // Kick of all requests
-    // Save data to localStorage
-
-    fetchLocation().then(location => {
-      request(`/tides?latitude=43.3845&longitude=-70.5440`).then(tides => {
-        this.setState({ tides: tides })
-        localStorage.setItem('tides', JSON.stringify(tides))
-      })
-
-      request(`/weather?latitude=43.3845&longitude=-70.5440`).then(weather => {
-        console.log(weather)
-        this.setState({ weather: weather })
-        localStorage.setItem('weather', JSON.stringify(weather))
-      })
-    })
-  }
-
-  // Derive your data don't save it
-
-  get nextTide() {
-    return findNextTide(this.state.tides)
-  }
-
   render() {
-    console.log(this.state)
-
-    if (!this.state.tides) {
-      return null
-    }
-
     return (
-      <div>
-        {this.state.tides && (
-          <TidePhrase city="Boston" nextTide={this.nextTide} />
-        )}
-        {this.state.weather && <CurrentWeather weather={this.state.weather} />}
-
-        <TodaysTides tides={this.state.tides} />
-      </div>
+      <Router>
+        <Route exact path="/" component={Location} />
+      </Router>
     )
   }
 }
---
+++ src/location/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+import request from '../utils/request'
+import findNextTide from '../utils/find-next-tide'
+import { fetchLocation } from '../utils/location'
+import TidePhrase from './tide-phrase'
+import CurrentWeather from './current-weather'
+import TodaysTides from './todays-tides'
+
+class Location extends Component {
+  state = {
+    tides: JSON.parse(localStorage.getItem('tides')),
+    weather: JSON.parse(localStorage.getItem('weather')),
+  }
+
+  componentDidMount() {
+    // Get user location
+    // Kick of all requests
+    // Save data to localStorage
+
+    fetchLocation().then(location => {
+      request(`/tides?latitude=43.3845&longitude=-70.5440`).then(tides => {
+        this.setState({ tides: tides })
+        localStorage.setItem('tides', JSON.stringify(tides))
+      })
+
+      request(`/weather?latitude=43.3845&longitude=-70.5440`).then(weather => {
+        console.log(weather)
+        this.setState({ weather: weather })
+        localStorage.setItem('weather', JSON.stringify(weather))
+      })
+    })
+  }
+
+  // Derive your data don't save it
+
+  get nextTide() {
+    return findNextTide(this.state.tides)
+  }
+
+  render() {
+    console.log(this.state)
+
+    if (!this.state.tides) {
+      return null
+    }
+
+    return (
+      <div className={'container'}>
+        {this.state.tides && (
+          <TidePhrase city="Boston" nextTide={this.nextTide} />
+        )}
+        {this.state.weather && <CurrentWeather weather={this.state.weather} />}
+
+        <TodaysTides tides={this.state.tides} />
+      </div>
+    )
+  }
+}
+
+export default Location
