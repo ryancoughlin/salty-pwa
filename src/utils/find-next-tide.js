6a65b394fd641ada23de92074f70f92acc452074
Ryan Coughlin <ryan@ryancoughlin.com>
2018-02-28 13:39:07 -0500

    ISSUE

--- src/utils/find-next-tide.js
+++ src/utils/find-next-tide.js
@@ -2,7 +2,6 @@ import _ from 'lodash'
 import moment from 'moment'
 
 export default function findNextTide(tides) {
-  console.log(tides)
   const allTides = _.flatten(_.values(tides))
   const nextTideIndex = _.findIndex(allTides, tide => {
     const tideTime = moment.utc(tide.time).local()
