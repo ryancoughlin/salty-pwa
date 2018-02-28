7ba8cf5ac97e688854e5c80a0c4e6c413682ed95
Ryan Coughlin <ryan@ryancoughlin.com>
2018-02-27 22:44:40 -0500

    Use text X for modal close

--- src/tide-tables/tide-arrow.js
+++ src/tide-tables/tide-arrow.js
@@ -4,9 +4,9 @@ import Icon from '../common/icon'
 import downArrow from '../assets/images/low-tide-arrow.png'
 import upArrow from '../assets/images/high-tide-arrow.png'
 
-const TideArrow = direction => <Icon source={tideArrow(direction)} />
+const TideArrow = direction => <Icon source={findDirection(direction)} />
 
-const tideArrow = ({ direction }) => {
+const findDirection = ({ direction }) => {
   console.log(direction)
   if (direction === 'high') {
     return upArrow
