f85ff5900f12690c0ae1f58b6bfc5ae8430a2b18
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-02 23:50:18 -0500

    Add router transitions (basic)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AnimatedSwitch } from 'react-router-transition'
 import Location from './location/index'
 import TideTables from './tide-tables/index'
 
@@ -7,10 +8,15 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
           <Route exact path="/" component={Location} />
           <Route exact path="/tables" component={TideTables} />
-        </div>
+        </AnimatedSwitch>
       </Router>
     )
   }
