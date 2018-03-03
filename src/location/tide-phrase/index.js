a585df392e46071c6d4d273c2fbee17f99527854
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-02 22:31:14 -0500

    Move over to a custom geocode request and remove broken one

--- src/location/tide-phrase/index.js
+++ src/location/tide-phrase/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import glamorous from 'glamorous'
-import { geoCodeLocation } from '../../utils/location'
+import geocodeLocation from '../../utils/geocode'
 import Styles from '../../assets/styles'
 import RemainingTideTime from './remaining-tide-time'
 import TideArrow from '../../common/tide-arrow'
@@ -35,8 +35,18 @@ const TidePhrase = class extends Component {
   }
 
   getCityName = () => {
-    geoCodeLocation(this.props.location)
-      .then(city => {
+    geocodeLocation(this.props.location)
+      .then(result => {
+        const cityComponents = result.results[0].address_components.filter(
+          function(addr) {
+            return addr.types[0] === 'locality'
+              ? 1
+              : addr.types[0] === 'administrative_area_level_1' ? 1 : 0
+          },
+        )
+
+        const city = cityComponents[0].long_name
+
         this.setState({ city: city })
         localStorage.setItem('city', JSON.stringify(city))
       })
