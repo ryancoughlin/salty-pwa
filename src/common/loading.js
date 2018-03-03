1597d8cb67c9e280e44fc56923db1b06eeff70ca
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-02 22:37:43 -0500

    Make loading component take up full width and height

--- src/common/loading.js
+++ src/common/loading.js
@@ -10,8 +10,8 @@ const Loading = () => (
 
 const Container = glamorous.div({
   display: 'flex',
-  width: '100%',
-  height: '100%',
+  width: '100vw',
+  height: '100vh',
   alignItems: 'center',
   justifyContent: 'center',
 })
