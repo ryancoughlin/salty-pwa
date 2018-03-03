b3163ec554934dc996840edef40566e6b659d63d
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-02 22:34:54 -0500

    Remove geocode from location utility

--- src/utils/location.js
+++ src/utils/location.js
@@ -1,27 +1,8 @@
-import Geocode from 'react-geocode'
-
 export function fetchLocation() {
   return new Promise(resolve => {
     navigator.geolocation.getCurrentPosition(
       position => {
         resolve(position.coords)
-      },
-      error => {
-        console.error(error)
-      },
-    )
-  })
-}
-
-export function geoCodeLocation(location) {
-  return new Promise(resolve => {
-    const { latitude, longitude } = location
-
-    Geocode.setApiKey('AIzaSyAB4Hyk0FUOkDXaBSZu0Q1NYOYjxG3Nh7E')
-    Geocode.fromLatLng(latitude, longitude).then(
-      response => {
-        const cityName = response.results[0].address_components[3].long_name
-        resolve(cityName)
       },
       error => {
         console.error(error)
