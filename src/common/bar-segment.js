08dd3d54920d258297d4fc12b062995a5cc6bfed
Ryan Coughlin <ryan@ryancoughlin.com>
2018-03-17 22:07:26 -0400

    Adjust transform origin on the bar segment

--- src/common/bar-segment.js
+++ src/common/bar-segment.js
@@ -15,8 +15,9 @@ class BarSegment extends React.Component {
         />
 
         <g
+          style={{ transformOrigin: '4px 6px' }}
           transform={`translate(${this.props.x - 4}, ${this.props.y0 +
-            20}) rotate(${this.props.datum.direction})`}
+            8}) rotate(${this.props.datum.direction})`}
         >
           <path
             d="M4.464 1.16L8 10H5L4 8l-1 2H0l3.536-8.84c.102-.256.393-.38.65-.278.127.05.227.152.278.28z"
