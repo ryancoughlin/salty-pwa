0968bf4f43a4557d72e62a483e5b5aed6ee64c8b
Ryan Coughlin <ryan@ryancoughlin.com>
2018-10-13 11:39:52 -0400

    Remove unused line

--- src/tide-tables/index.js
+++ src/tide-tables/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import Header from './header'
 import TideRow from './tide-row'
 import ModalHeader from '../common/modal-header'
