5346ab34300be994acb790326ebaffc5e5d0fb7b
Ryan Coughlin <ryan@ryancoughlin.com>
2018-11-07 10:40:26 -0500

    Remove lodash from find next tide

--- src/utils/find-next-tide.js
+++ src/utils/find-next-tide.js
@@ -1,12 +1,12 @@
-import _ from 'lodash';
 import moment from 'moment';
 
-export default function findNextTide(tides) {
-  const allTides = _.flatten(_.values(tides));
-  const nextTideIndex = _.findIndex(allTides, (tide) => {
+export default function findNextTide(data) {
+  const tideArray = Object.values(data);
+  const flattenedTides = tideArray.flat();
+  const nextTideIndex = tideArray.findIndex((tide) => {
     const tideTime = moment(tide.time);
     return moment().diff(tideTime) <= 0;
   });
 
-  return allTides[nextTideIndex];
+  return flattenedTides[nextTideIndex];
 }
