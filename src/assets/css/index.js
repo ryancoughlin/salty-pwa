258dcc30a86be952d87f12bdd219c742e118f4f0
Ryan Coughlin <ryan@ryancoughlin.com>
2018-11-06 21:12:49 -0500

    Move CSS imports

---
+++ src/assets/css/index.js
@@ -0,0 +1,2 @@
+import main from 'main.css'
+import webfonts from 'webfonts.css'
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
-import './index.css'
-import './webfonts.css'
+import './assets/css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
