7eace6436173620b643423ca79d36f9f6e6f737e
Ryan Coughlin <ryan@ryancoughlin.com>
2018-11-10 13:23:06 -0500

    Remove lodash for padStart

--- src/location/overview/remaining-tide-time.js
+++ src/location/overview/remaining-tide-time.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import UI from '../../assets/ui';
+import addLeadingZero from '../../utils/add-leading-zero';
 
 export default class RemainingTideTime extends Component {
   componentDidMount() {
@@ -16,20 +17,11 @@ export default class RemainingTideTime extends Component {
   get formattedTime() {
     const time = moment(this.props.nextTide.time);
     const diff = moment.duration(time.diff(moment()));
-    // console.log('TCL: RemainingTideTime -> getformattedTime -> this.props.nextTide', this.props.nextTide);
-    // console.log('TCL: RemainingTideTime -> getformattedTime -> time', time);
-    // console.log('TCL: RemainingTideTime -> getformattedTime -> diff.minutes()', diff.minutes());
-    // console.log('TCL: RemainingTideTime -> getformattedTime -> diff.hours()', diff.hours());
-
-    return `${this.padNumbers(diff.hours())}:${this.padNumbers(diff.minutes())}:${this.padNumbers(diff.seconds())} `;
+    return `${addLeadingZero(diff.hours(), 0)}:${addLeadingZero(diff.minutes(), 0)}:${addLeadingZero(diff.seconds(), 0)} `;
   }
 
   get formattedTideDirection() {
     return `UNTIL ${this.props.nextTide.type.toUpperCase()}`;
-  }
-
-  padNumbers(number) {
-    return number;
   }
 
   render() {
---
+++ src/utils/add-leading-zero.js
@@ -0,0 +1,2 @@
+const addLeadingZero = number => (`0${number}`).slice(-2);
+export default addLeadingZero;
