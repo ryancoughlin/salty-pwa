ddbf3eff82c0d8d3120724f6ba3460f0e5b2cf31
ryancoughlin <ryan@ryancoughlin.com>
2021-03-01 02:24:50 +0000

    stuff

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
-};
+}
 
 const request = function (path) {
   const params = {
@@ -9,20 +9,20 @@ const request = function (path) {
       ...DEFAULT_HEADERS,
     },
     method: 'get',
-  };
+  }
 
-  const response = fetch(`${process.env.API_URL}${path}`, params);
+  const response = fetch(`${process.env.API_URL}${path}`, params)
 
   response.catch((error) => {
-    console.error(error);
-  });
+    console.error(error)
+  })
 
   return response.then((res) => {
     if (res.ok) {
-      return res.json();
+      return res.json()
     }
-    return res.json().then(json => Promise.reject(json));
-  });
-};
+    return res.json().then((json) => Promise.reject(json))
+  })
+}
 
-export default request;
+export default request
